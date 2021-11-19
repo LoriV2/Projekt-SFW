9dcc06c3d8ae5175332ce466dc93424683c775f1
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2021-11-19 14:34:24 +0100

    Add files via upload

--- login.php
+++ login.php
@@ -1,13 +1,20 @@
 <html>
 <body>
-<?php
-$Login=$_POST['login'];
-$Pass=$_POST['password'];
-
-
-echo $Login;
-echo $Pass;
-?>
+  <?php
+ $has=$_POST['password'];
+ $log=$_POST['login'];
+  
+ $has = hash('sha256',$has);
+  $log = hash('sha256',$log);
+ $has = base64_encode($has);
+ $has = hash('sha256',$has);
+ $has = hash('sha256',$has);
+ echo $has;
+ echo "<br>";
+ echo $log;
+  
+  
+  ?>
 </body>
 
 
