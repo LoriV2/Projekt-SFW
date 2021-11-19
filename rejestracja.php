26ff5fcb2d6c79f2bc59572caed1371270c189ed
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2021-11-19 22:36:21 +0100

    Update rejestracja.php

--- rejestracja.php
+++ rejestracja.php
@@ -2,20 +2,21 @@
 <html lang="en">
 <head>
 <meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0"  http-equiv="X-UA-Compatible" content="ie=edge"/>
-
+<link rel='stylesheet' href='wygląd.css'>
   <title>Rejestracja</title>
 </head> 
 <body>
-  <form action="login.php" method="post"> 
- <label>Mail:</label>  <input type="text" name="mail" required /> 
+
+  <form action="login.php" method="post" class='wrapkfc'> 
+ <label>Mail:</label> <p/> <input type="text" name="mail" required /> 
  <br/>
-  <label>Nazwa użytkownika:</label> <input type="text" name="login" required/> 
+  <label>Nazwa użytkownika:</label> <p/><input type="text" name="login" required/> 
     <br/> 
- <label>Hasło:</label> <input type="password" name="password" required/> 
-    <br/>
-    <button type="submit">log in</button>
+ <label>Hasło:</label> <p/><input type="password" name="password" required/> 
+   
+  <br/> <br/> <button type="submit">Zarejestruj się</button>
   </form>
-  
+
 
 </body>
 </html>
