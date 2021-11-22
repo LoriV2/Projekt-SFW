fb014eaea13b7dd59b47e708df66a780afa4c334
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2021-11-22 12:05:57 +0100

    Update logowanie.php

--- logowanie.php
+++ logowanie.php
@@ -22,7 +22,7 @@ $log = hash('sha256',$log);
 
 $select = mysqli_query($conn, "SELECT * FROM `loginhaslo` WHERE `login` = '".$log."' AND `haslo` = '".$has."'  ") or exit(mysqli_error($conn));
 if(mysqli_num_rows($select)) {
-    exit('Witamy');
+    $SESSION=
 	mysqli_close($conn);
 }
 
