b704a188c85c6c6983b821cf6079ef5caac3d5c5
Emmeciarz <42807539+Emmeciarz@users.noreply.github.com>
2021-11-29 08:18:24 +0100

    Add files via upload

---
+++ PHP_SQL/rejestracja.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8" name="viewport" content="width=device-width, initial-scale=1.0"  http-equiv="X-UA-Compatible" content="ie=edge"/>
+<link rel='stylesheet' href='wygląd.css'>
+  <title>Rejestracja</title>
+</head> 
+<body>
+
+  <form action="login.php" method="post" class='wrapkfc'> 
+ <label>Mail:</label> <p/> <input type="text" name="mail" required /> 
+ <br/>
+  <label>Nazwa użytkownika:</label> <p/><input type="text" name="login" required/> 
+    <br/> 
+ <label>Hasło:</label> <p/><input type="password" name="password" required/> 
+   
+  <br/> <br/> <button type="submit">Zarejestruj się</button>
+  </form>
+
+
+</body>
+</html>
