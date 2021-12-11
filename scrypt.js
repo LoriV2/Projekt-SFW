b19d139502d89582513bc086c8404bf38cb10275
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2021-12-11 18:16:28 +0100

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -1,24 +1,13 @@
-var modal = document.getElementById('modal');
-var modal2 = document.getElementById('modal2');
+
 
 function pokaz() {
-    document.getElementById("wiesz").style.display = "block";
+    document.getElementById("LoginForm").style.display = "block";
   }
 
   function pokaz2(){
       document.getElementById("niewiesz").style.display="block"; 
        }
-window.onclick = function(event) {
-if (event.target == modal )  
-{
-  modal.style.display = "none";
-}
 
-if (event.target == modal2 )  
-{
-  modal2.style.display = "none";
-}
-}
 
 
 
