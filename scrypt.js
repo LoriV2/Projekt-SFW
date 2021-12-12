a00a5ab593b2dd77cd03a03ca2e5e3881cae9f92
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2021-12-12 02:57:43 +0100

    Update scrypt.js

--- scrypt.js
+++ scrypt.js
@@ -1,15 +1,40 @@
+let parameters={
+  ilosc : false,
+  litery : false,
+  cyfry : false,
+  specjalne : false
+}
+
+function hasłosprawdzacz(){
+ let haslo = document.getElementById('haslo').value;
 
 
-function pokaz() {
-    document.getElementById("LoginForm").style.display = "block";
-  }
 
-  function pokaz2(){
-      document.getElementById("niewiesz").style.display="block"; 
-       }
+parameters.cyfry = (/[0-9]+/.test(haslo))?
+true:false;
+parameters.specjalne = (/[!\'$%&/()=?@~`#\\.\';:+=^*_-]+/.test(haslo))?
+true:false;
+parameters.ilosc = (haslo.length > 7)?
+true:false;
+parameters.litery = (/[A-Za-z]+/.test(haslo))?
+true:false;
 
+if(parameters.cyfry === true){
+  document.getElementById("Liczby").style.color = "green";
+} else document.getElementById("Liczby").style.color = "red";
 
+if(parameters.specjalne === true){
+  document.getElementById("Specjalne").style.color = "green";
+} else document.getElementById("Specjalne").style.color = "red";
 
+if(parameters.ilosc === true){
+  document.getElementById("ilosc").style.color = "green";
+} else document.getElementById("ilosc").style.color = "red";
+
+if(parameters.litery === true){
+  document.getElementById("litery").style.color = "green";
+} else document.getElementById("litery").style.color = "red";
+}
 
 function onSignIn(googleUser) {
   // Useful data for your client-side scripts:
