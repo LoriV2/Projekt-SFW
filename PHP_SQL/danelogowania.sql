d333773a345714495bc3c08b47ae663696ba5a7e
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2021-12-12 02:57:33 +0100

    Update danelogowania.sql

--- PHP_SQL/danelogowania.sql
+++ PHP_SQL/danelogowania.sql
@@ -32,6 +32,8 @@ CREATE TABLE `loginhaslo` (
   `login` varchar(32) COLLATE utf8_bin NOT NULL,
   `haslo` varchar(64) COLLATE utf8_bin NOT NULL,
   `userimie` varchar(25) COLLATE utf8_bin NOT NULL,
+  `Imię` varchar(25) COLLATE utf8_bin NOT NULL,
+  `Nazwisko` varchar(25) COLLATE utf8_bin NOT NULL,
   `liczbalogowan` INT COLLATE utf8_bin NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
