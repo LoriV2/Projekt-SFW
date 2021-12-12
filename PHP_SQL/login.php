4f6c49328b3bbf8ec060eedf28bed987537d1626
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2021-12-12 02:57:37 +0100

    Update login.php

--- PHP_SQL/login.php
+++ PHP_SQL/login.php
@@ -5,10 +5,13 @@
 </head>
 <body class='wrapkfc'>
   <?php
- $mail=$_POST['mail'];
- $has=$_POST['password'];
- $log=$_POST['login'];
+ $mail=$_POST['email'];
+ $has=$_POST['haslo'];
+ $login=$_POST['Login'];
  $userimie=($_POST['userimie']);
+ $imie=($_POST['Imię']);
+ $nazwisko=($_POST['Nazwisko']);
+ $plec = ($_POST['Płeć']);
 
 	$servername = "localhost";
 	$database = "danelogowania";
@@ -17,30 +20,31 @@
 
   $conn = mysqli_connect($servername, $username, $password, $database);
 if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-};
+    die("Connection failed: " . $conn->connect_error);
+  };
 
-$select = mysqli_query($conn, "SELECT `mail` FROM `loginhaslo` WHERE `mail` = '".$_POST['mail']."'") or exit(mysqli_error($conn));
+$select = mysqli_query($conn, "SELECT `mail` FROM `loginhaslo` WHERE `mail` = '".$mail."'") or exit(mysqli_error($conn));
 if(mysqli_num_rows($select)) {
-    exit('Ten email jest już zarejestrowany');
-	mysqli_close($conn);
-}  
+      exit('Ten email jest już zarejestrowany');
+    mysqli_close($conn);
+  }  
 
 $imie = mysqli_query($conn, "SELECT `userimie` FROM `loginhaslo` WHERE `userimie` = '".$userimie."'") or exit(mysqli_error($conn));
 if(mysqli_num_rows($select)) {
-    exit('Ta nazwa użytkownika jest już zajęta');
-	mysqli_close($conn);
-}  
+      exit('Ta nazwa użytkownika jest już zajęta');
+    mysqli_close($conn);
+  }  
 
+if ($plec === 'płeć'){ $plec = null; }
  
  $has = hash('sha256',$has);
  $has = base64_encode($has);
  $has = hash('sha256',$has);
  $has = hash('sha256',$has);
- $log = hash('sha256',$log);
+ $login = hash('sha256',$login);
 
-$sql = "INSERT INTO loginhaslo (mail,login,haslo,userimie)
-VALUES ('$mail','$log','$has','$userimie')";
+$sql = "INSERT INTO loginhaslo (Płeć,mail,login,haslo,userimie,Imię,Nazwisko)
+VALUES ('$plec','$mail','$login','$has','$userimie','$imie','$nazwisko')";
 
 if ($conn->query($sql) === TRUE) {
   echo "Pomyślnie zostałeś zarejestrowany";
