408884202aadbf062b7b2bbeaa239de87156a09d
LoriV2 <94517486+LoriV2@users.noreply.github.com>
2021-12-13 21:45:17 +0100

    Update logowanie.php

--- PHP_SQL/logowanie.php
+++ PHP_SQL/logowanie.php
@@ -1,71 +1,40 @@
 <html>
+
 <body>
 
-<?php
-session_start();
+	<?php
+	session_start();
 	$servername = "localhost";
 	$database = "danelogowania";
 	$username = "root";
 	$password = "";
 
-  $conn = mysqli_connect($servername, $username, $password, $database);
-if ($conn->connect_error) {
-die("Connection failed: " . $conn->connect_error);
-}
+	$has = $_POST['haslo'];
+	$login = $_POST['Login'];
 
-$has = $_POST['haslo'];
-$login = $_POST['Login'];
-$userimie = '';
+	$has = hash('sha256', $has);
+	$has = base64_encode($has);
+	$has = hash('sha256', $has);
+	$has = hash('sha256', $has);
+	$login = hash('sha256', $login);
 
-$has = hash('sha256',$has);
-$has = base64_encode($has);
-$has = hash('sha256',$has);
-$has = hash('sha256',$has);
-$login = hash('sha256',$login);
-
-
-if(!empty($log) && !empty($password) && !is_numeric($log))
-{
-
-	//czyta z bazy danych
-	$uzytkownik = mysqli_query($conn, "SELECT userimie FROM `loginhaslo` WHERE login='$log'");
-
-	if (!$uzytkownik) {
-		echo 'Could not run query: ' . mysqli_error($uzytkownik);
-		exit;
+	$conn = mysqli_connect($servername,  $username, $password, $database);
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
 	}
-	$row = mysqli_fetch_row($uzytkownik);
 	
-	$row[1] = $userimie;
-
-	$pytanie = "SELECT * FROM `loginhaslo` WHERE login = '$log' limit 1";
-
-	$popytaniu = mysqli_query($conn, $pytanie);
-
-	if($pytanie)
-	{
-		if($pytanie && mysqli_num_rows($popytaniu) > 0)
-		{
-			$dane = mysqli_fetch_assoc($popytaniu);
-			if($dane['haslo'] === $has)
-			{
-				
-				$_SESSION['NAZWAUZ'] = $dane['userimie'];
-				$conn->close();
-				echo "<script>window.top.location='../HTML/index.html'</script>";
-				
-			}else echo 'Błędne danie logowania';
-		} 
+	$sql = "SELECT * FROM `loginhaslo` WHERE `Login` = '$login' AND `haslo` = '$has'";
 	
+	$result = mysqli_query($conn, $sql);
+	$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+	
+	$count = mysqli_num_rows($result);
+	echo $count;
+	if ($count == 1) {
+		echo "<h1><center> Login successful </center></h1>";
+	} else {
+		echo "<h1> Login failed. Invalid username or password.</h1>";
 	}
-}
-
-
-
-
-
-
-
 ?>
 </body>
 
